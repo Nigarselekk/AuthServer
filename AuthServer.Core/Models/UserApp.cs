092bcdc17e5f0b92b576bc9c023906d5f1a5dc8c
Nigarselekk <nigarselek923@gmail.com>
2024-09-29 16:03:27 +0300

    Create entities

---
+++ AuthServer.Core/Models/UserApp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthServer.Core.Models
+{
+	public class UserApp :IdentityUser
+	{
+		public string City { get; set; }
+
+
+	}
+}
+
