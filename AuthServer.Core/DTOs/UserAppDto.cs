6e0722fc88c136ac15fbc11892fa17970383649e
Nigarselekk <nigarselek923@gmail.com>
2024-10-09 19:18:14 +0300

    feat: add IUserService interface and User Dto

---
+++ AuthServer.Core/DTOs/UserAppDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace AuthServer.Core.DTOs
+{
+	public class UserAppDto
+	{
+
+		public int  Id { get; set; }
+
+		public string UserName { get; set; }
+
+		public string Email { get; set; }
+
+		public string City { get; set; }
+
+
+	}
+}
+
